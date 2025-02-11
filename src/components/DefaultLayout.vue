1cd3508f3e131142d7f1fd1983f2d3f551cebbf6
arnold-aswan <fredyaswan@gmail.com>
2025-02-11 16:35:46 +0300

    app layout

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from './Navbar.vue';
+
+</script>
+
+
+<template>
+  <div class="bg-gray-100 space-y-5 h-full">
+    <navbar />
+    <router-view />
+  </div>
+</template>
